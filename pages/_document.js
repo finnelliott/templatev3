ede37add11f744a6e3c010699d34081da479727c
finnelliott <f.j.elliott@icloud.com>
2021-09-12 11:25:39 +0200

    fixing styling issue

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,5 @@
 // pages/_document.js
 import Document, { Html, Head, Main, NextScript } from 'next/document'
-import { InitializeColorMode } from 'theme-ui'
 
 import { createResolver } from 'next-slicezone/resolver'
 
@@ -16,7 +15,6 @@ export default class extends Document {
       <Html>
         <Head />
         <body>
-          <InitializeColorMode />
           <Main />
           <NextScript />
         </body>
